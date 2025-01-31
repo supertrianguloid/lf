d963ccad57f27d169309b780bf61761efb9b30e3
Laurence Sebastian Bowes <github@supertrianguloid.com>
2025-01-31 00:37:27 +0000

    Rename command line arguments to have more sensible names

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod io;
 mod statistics;
-use io::{load_channel_from_file_folded, load_global_t_from_file, load_wf_observable_from_file};
+use io::{load_channel_from_file_folded, load_global_t_from_file};
 use serde::{Deserialize, Serialize};
 use spectroscopy::effective_mass;
 use statistics::{mean, standard_deviation};
@@ -20,6 +20,7 @@ pub struct App {
     #[clap(subcommand)]
     command: Command,
 }
+
 #[derive(Subcommand, Debug)]
 enum Command {
     /// Calculate the effective mass in a given channel
@@ -39,9 +40,9 @@ struct ComputeEffectiveMassArgs {
     hmc_filename: String,
     #[arg(short, long, value_name = "CHANNEL")]
     channel: String,
-    #[arg(short, long, value_name = "NUM_CONFS", default_value_t = 0)]
+    #[arg(short, long, value_name = "THERMALISATION", default_value_t = 0)]
     thermalisation: usize,
-    #[arg(short, long, value_name = "NUM_SAMPLES", default_value_t = 1000)]
+    #[arg(short, long, value_name = "BOOTSTRAP_SAMPLES", default_value_t = 1000)]
     n_boot: u32,
     #[arg(short, long, value_name = "BIN_WIDTH", default_value_t = 10)]
     binwidth: usize,
@@ -156,12 +157,5 @@ fn compute_effective_mass_command(args: ComputeEffectiveMassArgs) {
         })
         .unwrap();
         wtr.flush().unwrap();
-        /*         println!(
-            "{:0>2},{},{},{}",
-            tau,
-            effmass_mean[tau - 1],
-            effmass_error[tau - 1],
-            solve_failures[tau - 1] as f64 * 100.0 / args.n_boot as f64
-        ); */
     }
 }
