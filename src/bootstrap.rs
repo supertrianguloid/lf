7031683df4ad99640a537d334d1e79cd4e77ab0e
Laurence Sebastian Bowes <github@supertrianguloid.com>
2025-04-06 20:54:16 +0100

    Chore: Bump `rand` to 0.9

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -1,8 +1,9 @@
-use rand::distributions::{Distribution, Uniform};
+use rand::distr::{Distribution, Uniform};
 pub fn get_samples(length: usize, binsize: usize) -> Vec<usize> {
     let length_new = length / binsize;
-    let mut rng = rand::thread_rng();
-    let samples: Vec<_> = Uniform::from(0..length)
+    let mut rng = rand::rng();
+    let samples: Vec<_> = Uniform::try_from(0..length)
+        .unwrap()
         .sample_iter(&mut rng)
         .take(length_new)
         .collect();
@@ -14,6 +15,7 @@ pub fn get_samples(length: usize, binsize: usize) -> Vec<usize> {
 fn test_bootstrap_binning_samples() {
     assert_eq!(get_samples(100, 2).len(), 50);
 }
+#[test]
 fn test_bootstrap_no_binning_samples() {
     assert_eq!(get_samples(100, 1).len(), 100);
 }
